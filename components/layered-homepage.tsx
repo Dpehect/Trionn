ebe5032d0a4f11f71ea357baf0264d713da228a9
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2026-07-26 15:35:22 +0300

    home delete l3

--- components/layered-homepage.tsx
+++ components/layered-homepage.tsx
@@ -7,7 +7,6 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
-import { BrandStatementSection } from "./brand-statement-section";
 import { CapabilityMarqueeSection } from "./capability-marquee-section";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
@@ -433,8 +432,6 @@ export function LayeredHomepage() {
           ))}
         </div>
       </section>
-
-      <BrandStatementSection />
 
       <CapabilityMarqueeSection />
     </main>
